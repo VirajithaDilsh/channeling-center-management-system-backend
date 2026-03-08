4aa77102272ab85d53223f00591dcf4b7c0dda5f
Virajitha Dilshan <virajitha.Jayathilaka@gmail.com>
2026-03-08 10:07:04 +0530

    medicine backend

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 require("dotenv").config();
 
+const medicineRoutes = require("./routes/medicineRoutes"); // import routes
+
 const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// database connection
 connectDB();
 
-// Use routes
+// routes
 app.use("/api", require("./routes/authRoute"));
+app.use("/api/medicines", medicineRoutes);
+
+// test route
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+);
