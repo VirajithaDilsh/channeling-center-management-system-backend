9f8662e027c90342f1d767a46729c343fd661981
Virajitha Dilshan <virajitha.Jayathilaka@gmail.com>
2026-06-25 17:49:07 +0000

    medice route change

--- controllers/medicineController.js
+++ controllers/medicineController.js
@@ -1,30 +1,53 @@
-import Medicine from "../models/Medicine.js";
+const Medicine = require("../models/Medicine");
 
-export const addMedicine = async (req, res) => {
+// CREATE
+exports.addMedicine = async (req, res) => {
     try {
         const medicine = new Medicine(req.body);
-
         const savedMedicine = await medicine.save();
-
         res.status(201).json(savedMedicine);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
-export const getMedicines = async (req, res) => {
+// READ
+exports.getMedicines = async (req, res) => {
     try {
         const medicines = await Medicine.find();
-
         res.json(medicines);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
-export const deleteMedicine = async (req, res) => {
+// UPDATE
+exports.updateMedicine = async (req, res) => {
     try {
-        await Medicine.findByIdAndDelete(req.params.id);
+        const updated = await Medicine.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if (!updated) {
+            return res.status(404).json({ message: "Medicine not found" });
+        }
+
+        res.json(updated);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// DELETE
+exports.deleteMedicine = async (req, res) => {
+    try {
+        const deleted = await Medicine.findByIdAndDelete(req.params.id);
+
+        if (!deleted) {
+            return res.status(404).json({ message: "Medicine not found" });
+        }
 
         res.json({ message: "Medicine deleted" });
     } catch (error) {
